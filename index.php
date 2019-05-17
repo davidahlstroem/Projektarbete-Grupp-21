b7da4d1432e5b7bd988bfba86a342256d1c74fd6
Unknox1 <50732205+Unknox1@users.noreply.github.com>
2019-05-17 10:35:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html>
     <head>
-            <meta charset="utf-8">
-            <link rel="stylesheet" href="css/main.css">
-                 
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/main.css">
+        <title></title>    
     </head>
-
     <body>
-            <ul>
+        <header>
+            <nav>
+                <h1>Välkommen hit</h1>
+                <ul>
                     <li><a class="active" href="#start">Startsida</a></li>
                     <li><a href="#produkt">Produktsida</a></li>
                     <li><a href="#kontakt">Kontakta oss</a></li>
                     <li><a href="#order">Ordersida</a></li>
-               </ul>
-
-            <h1>Välkommen hit</h1>
-       
+                </ul>
+            </nav>
+        </header>
     </body>
-
 </html>
