a72c46b176725dbb5fc241c9eef3cfb21c8260af
Unknox1 <50732205+Unknox1@users.noreply.github.com>
2019-05-21 22:25:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,27 +2,13 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="css/main.css">
-        <title></title>    
+        <link rel="stylesheet" href="assets/css/main.css">
+        <title></title>
     </head>
     <body>
-        <header>
-            <div class="header">
-                <h1>Header</h1>
-            </div>
-            <nav>
-                <h1>Välkommen hit</h1>
-                <ul>
-                    <li><a class="active" href="#start">Startsida</a></li>
-                    <li><a href="#produkt">Produktsida</a></li>
-                    <li><a href="#kontakt">Kontakta oss</a></li>
-                    <li><a href="#order">Ordersida</a></li>
-                    <li style="float:right"><a id="login" href="#login">Logga in</a></li>
-                </ul>
-            </nav>
-        </header>
-        <div class="footer">
-            <p>Footer - kontakt osv</p>
-        </div>       
+      <?php include "assets/html/header.php" ?>
+
+
+      <?php include "assets/html/footer.php" ?>
     </body>
 </html>
