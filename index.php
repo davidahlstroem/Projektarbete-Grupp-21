a3cf776a1edae4afdccbed673ded9aade81519f2
emmawestin1 <50732177+emmawestin1@users.noreply.github.com>
2019-05-22 10:51:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,15 +8,8 @@
     <body>
       <?php include "assets/html/header.php" ?>
         
-        <ul>
-        <li><a class="active" href="index.php">Startsida</a></li>
-        <li><a href="products.php">Produktsida</a></li>
-        <li><a href="register.php">Registrera dig</a></li>
-        <li><a href="order.php">Ordersida</a></li>
-        <li style="float:right"><a id="login" href="login.php">Logga in</a></li>
-        </ul>
-
-
+        <h1>Hallo</h1>
+        
       <?php include "assets/html/footer.php" ?>
     </body>
 </html>
