48705e859c4bfa25402772d8a00e81fa40ee2d20
Unknox1 <50732205+Unknox1@users.noreply.github.com>
2019-05-22 11:30:52 +0200

    Update register.php

--- register.php
+++ register.php
@@ -10,10 +10,10 @@
 
         <form class="regForm" action="" method="post">
         <label>First name:</label>
-        <input type="firstName" name="firstName" placeholder="Enter first name...">
+        <input type="text" name="firstName" placeholder="Enter first name...">
         <br>
         <label>Last name:</label>
-        <input type="lastName" name="lastName" placeholder="Enter last name...">
+        <input type="text" name="lastName" placeholder="Enter last name...">
         <br>
         <label>Email:</label>
         <input type="email" name="email" placeholder="Enter email...">
@@ -22,7 +22,7 @@
         <input type="password" name="pwd" placeholder="Enter password...">
         <br>
         <label>Confirm password:</label>
-        <input type="confirmPassword" name="confirmPwd" placeholder="Enter password again...">
+        <input type="password" name="confirmPwd" placeholder="Enter password again...">
         <button type="submit" name="btnReg">Register</button>
         </form>
 
