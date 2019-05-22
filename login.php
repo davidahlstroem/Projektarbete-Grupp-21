5024cb20aae76826a39fb48296e0c88ef6e109f6
emmawestin1 <50732177+emmawestin1@users.noreply.github.com>
2019-05-22 10:46:30 +0200

    Update login.php

--- login.php
+++ login.php
@@ -8,14 +8,6 @@
     <body>
       <?php include "assets/html/header.php" ?>
 
-        <ul>
-        <li><a href="index.php">Startsida</a></li>
-        <li><a href="products.php">Produktsida</a></li>
-        <li><a href="register.php">Registrera dig</a></li>
-        <li><a href="order.php">Ordersida</a></li>
-        <li style="float:right"><a id="login" class="active" href="login.php">Logga in</a></li>
-        </ul>
-
         <form class="loginForm" action="" method="post">
           <label>Email:</label>
           <input type="email" name="email" placeholder="Enter email...">
