77d755aafd7b440e47d29d9a1af789b73189922b
emmawestin1 <50732177+emmawestin1@users.noreply.github.com>
2019-05-22 10:49:07 +0200

    Update products.php

--- products.php
+++ products.php
@@ -8,15 +8,7 @@
     <body>
       <?php include "assets/html/header.php" ?>
 
-        <ul>
-        <li><a href="index.php">Startsida</a></li>
-        <li><a class="active" href="products.php">Produktsida</a></li>
-        <li><a href="register.php">Registrera dig</a></li>
-        <li><a href="order.php">Ordersida</a></li>
-        <li style="float:right"><a id="login" href="login.php">Logga in</a></li>
-        </ul>
-
-        <h2>test</h2>
+        <h2>test produkt</h2>
 
       <?php include "assets/html/footer.php" ?>
     </body>
