48bdc0b4ecbd4445b717ca3b1060a90340e08b9a
emmawestin1 <50732177+emmawestin1@users.noreply.github.com>
2019-05-24 11:51:33 +0200

    Update products.php

--- products.php
+++ products.php
@@ -3,26 +3,30 @@
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="assets/css/main.css">
-        <link rel="stylesheet" href="assets/css/product-info.css">
+        <link rel="stylesheet" href="assets/css/productinfo.css">
         <title></title>
     </head>
     <body>
       <?php include "assets/html/header.php" ?>
 
         <div class="product-overview">
+          <div>
           <img src="assets/img/body/st_black_body.jpg" alt="product-picture">
-          <div class="product-title">
-            <h2>Product name</h2>
-            <h4>art nr</h4>
           </div>
-          <div class="product-text">
-            <p>Description</p>
-          </div>
-          <div class="product-price">
-            <p>Pris!</p>
-          </div>
-          <div class="product-order">
-            <a href="#orderPage">Order!</a>
+          <div class="productinfo">
+            <div class="product-title">
+              <h2>Product name</h2>
+              <h4>art nr</h4>
+            </div>
+            <div class="product-text">
+              <p>Following the wild success of our 070, players demanded we adapt the sleek, modern design to a 6 string. The 060 is the result! This guitar is everything great about our instruments in one package.</p>
+            </div>
+            <div class="product-price">
+              <p>Pris!</p>
+            </div>
+            <div class="product-order">
+              <a href="#orderPage">Order!</a>
+            </div>
           </div>
         </div>
 
