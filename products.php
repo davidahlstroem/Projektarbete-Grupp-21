4a637e6dfcde7a5366fc0199f1cc655c0e496867
Unknox1 <50732205+Unknox1@users.noreply.github.com>
2019-05-24 11:11:45 +0200

    Update products.php

--- products.php
+++ products.php
@@ -1,14 +1,30 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <link rel="stylesheet" href="assets/css/main.css">
-    <meta charset="utf-8">
-    <title>productPage</title>
-  </head>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="assets/css/main.css">
+        <link rel="stylesheet" href="assets/css/product-info.css">
+        <title></title>
+    </head>
     <body>
       <?php include "assets/html/header.php" ?>
 
-        <h2>test produkt</h2>
+        <div class="product-overview">
+          <img src="assets/img/body/st_black_body.jpg" alt="product-picture">
+          <div class="product-title">
+            <h2>Product name</h2>
+            <h4>art nr</h4>
+          </div>
+          <div class="product-text">
+            <p>Description</p>
+          </div>
+          <div class="product-price">
+            <p>Pris!</p>
+          </div>
+          <div class="product-order">
+            <a href="#orderPage">Order!</a>
+          </div>
+        </div>
 
       <?php include "assets/html/footer.php" ?>
     </body>
