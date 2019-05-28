258b0ed619f00e651789d45742a2e8a2849212cc
erikbrasar <50731982+erikbrasar@users.noreply.github.com>
2019-05-28 16:49:11 +0200

    Add files via upload

---
+++ Include/html/commentForm.php
@@ -0,0 +1,13 @@
+<div class="addComment">
+    <br>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <form method="post" onsubmit="" action="comment.php">  
+        Comment:
+        <br>
+        <textarea rows="4" cols="50" id="comt" name="cmt" placeholder="Max 300 characters"></textarea>
+        <br>
+        <input type="submit" value="Comment">
+
+    </form>
+    <br>
+</div>
