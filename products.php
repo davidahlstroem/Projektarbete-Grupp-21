75026ed184423128f605182a19e8139ad7686cd0
Unknox1 <50732205+Unknox1@users.noreply.github.com>
2019-05-28 12:13:43 +0200

    Update products.php

--- products.php
+++ products.php
@@ -1,3 +1,4 @@
+<?php require "include/php/functions.php"; ?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -11,22 +12,11 @@
 
         <div class="product-overview">
           <div>
-          <img src="assets/img/body/st_black_body.jpg" alt="product-picture">
+            <img src="assets/img/body/st_black_body.jpg" alt="product-picture">
           </div>
-          <div class="productinfo">
-            <div class="product-title">
-              <h2>Product name</h2>
-              <h4>art nr</h4>
-            </div>
-            <div class="product-text">
-              <p>Following the wild success of our 070, players demanded we adapt the sleek, modern design to a 6 string. The 060 is the result! This guitar is everything great about our instruments in one package.</p>
-            </div>
-            <div class="product-price">
-              <p>Pris!</p>
-            </div>
-            <div class="product-order">
-              <a href="order.php">Order!</a>
-            </div>
+          <?php displayProductInfo(101321); ?>
+          <div class="product-order">
+            <a href="#orderPage">Order!</a>
           </div>
         </div>
 
