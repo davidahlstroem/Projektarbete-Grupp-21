0a920928152d1e2bbcb69faf5304adf5bfc572c2
Unknox1 <50732205+Unknox1@users.noreply.github.com>
2019-05-29 18:27:03 +0200

    Add files via upload

---
+++ Include/html/footer.php
@@ -0,0 +1,3 @@
+<div class="footer">
+  <h5>Kotakta oss:<br>018-12 34 56</h5>
+</div>
---
+++ Include/html/header.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+require "include/php/functions.php";
+?>
+<header>
+  <div class="header">
+    <h1>Header</h1>
+  </div>
+  <nav>
+    <ul>
+        <li><a class="active" href="index.php">Startsida</a></li>
+        <li><a href="products.php">Produktsida</a></li>
+        <li><a href="register.php">Registrera dig</a></li>
+        <li><a href="order.php">Ordersida</a></li>
+        <li><a href="comments.php">Kommentarer</a></li>
+        <li style='float:right'><?php displayLogin(); ?></li>
+    </ul>
+  </nav>
+</header>
