5c7433c330a2d877944444a6f6a428e8ceb31979
Unknox1 <50732205+Unknox1@users.noreply.github.com>
2019-05-29 18:13:42 +0200

    Update register.php

--- register.php
+++ register.php
@@ -9,27 +9,27 @@
       <?php include "include/html/header.php" ?>
 
         <form class="regForm" action="include/php/processRegistration.php" method="post">
-        <div>
-        <label>First name:</label>
-        <input type="text" name="firstName" placeholder="Enter first name...">
-        </div>
-        <div>
-        <label>Last name:</label>
-        <input type="text" name="lastName" placeholder="Enter last name...">
-        </div>
-        <div>
-        <label>Email:</label>
-        <input type="email" name="email" placeholder="Enter email...">
-        </div>
-        <div>
-        <label>Password:</label>
-        <input type="password" name="pwd" placeholder="Enter password...">
-        </div>
-        <div>
-        <label>Confirm password:</label>
-        <input type="password" name="confirmPwd" placeholder="Enter password again...">
-        </div>
-        <button type="submit" name="btnReg">Register</button>
+          <div>
+            <label>First name:</label>
+            <input type="text" name="firstName" placeholder="Enter first name...">
+          </div>
+          <div>
+            <label>Last name:</label>
+            <input type="text" name="lastName" placeholder="Enter last name...">
+          </div>
+          <div>
+            <label>Email:</label>
+            <input type="email" name="email" placeholder="Enter email...">
+          </div>
+          <div>
+            <label>Password:</label>
+            <input type="password" name="pwd" placeholder="Enter password...">
+          </div>
+          <div>
+            <label>Confirm password:</label>
+            <input type="password" name="confirmPwd" placeholder="Enter password again...">
+          </div>
+          <button type="submit" name="btnReg">Register</button>
         </form>
 
       <?php include "include/html/footer.php" ?>
