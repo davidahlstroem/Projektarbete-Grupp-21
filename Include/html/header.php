c8f818b1bc7206e1818c01e77878c45eacd84d65
emmawestin1 <50732177+emmawestin1@users.noreply.github.com>
2019-06-03 13:58:57 +0200

    Update header.php

--- Include/html/header.php
+++ Include/html/header.php
@@ -12,9 +12,12 @@ require "include/php/functions.php";
         <li><a href="products.php">Products</a></li>
         <li><a href="register.php">Register</a></li>
         <li><a href="order.php">Order</a></li>
-
+        <li><a href="map.php">Find our store</a></li>
+        <li><form action = "search.php" method="POST">
+        <input class="search" type="text" name="search" placeholder="Search...">
+        <button class="searchBtn" type="submit" name="submit-search" value="Enter"></button>
+        </form></li>
         <li style='float:right'><?php displayLogin(); ?></li>
-        <li style='float:right'><a href="cart.php">Shopping Cart</a></li>
     </ul>
   </nav>
 </header>
