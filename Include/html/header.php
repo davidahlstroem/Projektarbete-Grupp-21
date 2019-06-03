a21cee5f8bf8c4e35ecede0955bb472f55e5961c
Unknox1 <50732205+Unknox1@users.noreply.github.com>
2019-06-03 19:44:36 +0200

    Update header.php

--- Include/html/header.php
+++ Include/html/header.php
@@ -9,12 +9,14 @@ require "include/php/functions.php";
   <nav>
     <ul>
         <li><a class="active" href="index.php">Home</a></li>
-        <li style='float:right'><a href="register.php">Register</a></li>
         <li><a href="map.php">Find our store</a></li>
-        <li><form action = "search.php" method="POST">
-        <input class="search" type="text" name="search" placeholder="Search...">
-        <button class="searchBtn" type="submit" name="submit-search" value="Enter"></button>
-        </form></li>
+        <li>
+          <form class="search-form" action = "search.php" method="POST">
+            <input class="search" type="text" name="search" placeholder="Search...">
+            <button class="searchBtn" type="submit" name="submit-search" value="Enter"></button>
+          </form>
+        </li>
+        <?php if(!isset($_SESSION['email'])) echo("<li style='float:right'><a href='register.php'>Register</a></li>"); ?>
         <li style='float:right'><?php displayLogin(); ?></li>
         <li style='float:right'><?php displayShoppingCart(); ?></li>
     </ul>
