1efd6edbd2857be982b0274e690116f37a1aa8b6
Unknox1 <50732205+Unknox1@users.noreply.github.com>
2019-06-04 08:46:57 +0200

    Update order.php

--- order.php
+++ order.php
@@ -2,8 +2,10 @@
 <html lang="en" dir="ltr">
   <head>
     <link rel="stylesheet" href="assets/css/order.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/cart.css">
     <meta charset="utf-8">
-    <title>productPage</title>
+    <title>OrderPage</title>
   </head>
     <body>
       <?php include "include/html/header.php" ?>
@@ -11,10 +13,6 @@
         <h2>order</h2>
 
         <?php displayOrderInfo(); ?>
-
-
-
-
 
 
         <br>
