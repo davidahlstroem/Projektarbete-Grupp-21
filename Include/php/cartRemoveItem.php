2709bc91bfa8d38901487307c2a573fbf06dac3a
Unknox1 <50732205+Unknox1@users.noreply.github.com>
2019-06-04 08:52:22 +0200

    Update cartRemoveItem.php

--- Include/php/cartRemoveItem.php
+++ Include/php/cartRemoveItem.php
@@ -5,17 +5,16 @@ if (isset($_POST['cartRemoveBtn'])) {
   if(count($_SESSION['shoppingCart']) > 0){
     foreach ($_SESSION['shoppingCart'] as $key => $value) {
       if($value == $_GET['artNo']){
-        unset($_SESSION['shoppingCart'][$key]);
+        //$temp = $_SESSION['shoppingCart'][$key]; ||
+        $_SESSION['shoppingCart'][$key] =  $_SESSION['shoppingCart'][count($_SESSION['shoppingCart'])-1];
         array_pop($_SESSION['shoppingCart']);
-        break;
+        //unset($_SESSION['shoppingCart');
       }
     }
     header("Location: ../../cart.php");
   } else {
-    unset($_SESSION['shoppingCart']);
+      unset($_SESSION['shoppingCart']);
+      header("Location: ../../cart.php");
+    }
   }
-  }
-
-
-
 ?>
